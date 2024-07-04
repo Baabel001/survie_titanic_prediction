9b0b04877177e1fcf42b472a0d0c50b76a6f149f
Baabel001 <charles.thiam14@gmail.com>
2024-07-04 12:51:28 +0200

    logistic model and evaluation

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,10 +1,10 @@
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.metrics import ConfusionMatrixDisplay
-
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, ConfusionMatrixDisplay, roc_curve, auc
+import matplotlib.pyplot as plt
 
 def accu_display(y, y_pred):
     accuracy = accuracy_score(y, y_pred)
     print(f'Accuracy: {accuracy}\n\nClassification Report:\n{classification_report(y, y_pred)}')
+
     
 def confusion_matrix(y, y_pred):
     
@@ -13,3 +13,18 @@ def confusion_matrix(y, y_pred):
     disp = ConfusionMatrixDisplay(confusion_matrix = conf_matrix,
                                  display_labels=["Survived", "Not Survived"])
     disp.plot()
+
+    
+def roc(y, y_pred):
+    
+    fpr , tpr, thresholds = roc_curve(y, y_pred)
+
+    plt.plot([0,1],[0,1], 'k--')
+    plt.plot(fpr, tpr)
+    plt.legend()
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.title('Receiver Operating Characteristic')
+    plt.show()
+
+    print("Area under Roc Curve for SVM: ", "%.3f" % auc(fpr, tpr))
